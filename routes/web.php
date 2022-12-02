1011ad6e2bd0ca10abb5140d7ab5e5256fa6e629
Slava42 <slava.bogomazov14@gmail.com>
2022-12-01 23:05:38 -0800

    i7yui6uiyui

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 use Illuminate\Support\Facades\Route;
-
-
 Route::post('/test/result', [App\Http\Controllers\TestController::class, 'result']);
 Route::match(['get','post'],'/test/form/', [App\Http\Controllers\TestController::class, 'form']);
 
